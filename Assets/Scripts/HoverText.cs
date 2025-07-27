64f0bd97b3ab32db70f948876d324939d7b82237
Austin Hwang <46493950+austinhwangjr@users.noreply.github.com>
2025-07-27 14:29:15 +0800

    [-] Added some public vars to HoverText script
    Can change textbox bg color now

--- Assets/Scripts/HoverText.cs
+++ Assets/Scripts/HoverText.cs
@@ -5,16 +5,25 @@ using UnityEngine.EventSystems;
 public class HoverText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     //TextMeshPro textmeshpro;
+    public SpriteRenderer bg_sprite_renderer;
+    public Color initial_bg_color;
+    public Color hover_bg_color;
+    public Color initial_text_color;
+    public Color hover_text_color;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        GetComponent<TextMeshPro>().color = Color.cyan;
-        GetComponent<TextMeshPro>().fontStyle = FontStyles.Underline;
+        bg_sprite_renderer.color = hover_bg_color;
+        GetComponent<TextMeshPro>().color = hover_text_color;
+        //GetComponent<TextMeshPro>().color = Color.cyan;
+        //GetComponent<TextMeshPro>().fontStyle = FontStyles.Underline;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        GetComponent<TextMeshPro>().color = Color.black;
-        GetComponent<TextMeshPro>().fontStyle = FontStyles.Normal;
+        bg_sprite_renderer.color = initial_bg_color;
+        GetComponent<TextMeshPro>().color = initial_text_color;
+        //GetComponent<TextMeshPro>().color = Color.black;
+        //GetComponent<TextMeshPro>().fontStyle = FontStyles.Normal;
     }
 }
