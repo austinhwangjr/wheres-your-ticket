76ba5110cce01c7ed2ea0a3983e3c13f68654501
Austin Hwang <46493950+austinhwangjr@users.noreply.github.com>
2026-03-13 11:30:34 +0800

    [-] Added a desktop manager, preparing to implement desktop behaviour

---
+++ Assets/Scripts/Data/TicketIssueType.cs
@@ -0,0 +1,16 @@
+/**
+ * TicketIssueType.cs
+ * 
+ * This script manages the different types of issues that can be reported in the game.
+ * 
+ * @author Austin Hwang
+ * @date 13 March 2026
+ */
+
+public enum IssueType
+{
+    None,
+    WifiNoInternet,
+    VpnCannotConnect,
+    OutlookNotOpening
+}
---
+++ Assets/Scripts/Managers/UserDesktopManager.cs
@@ -0,0 +1,34 @@
+/**
+ * UserDesktopManager.cs
+ * 
+ * This script manages the "Remote Desktop" mode, which simulates the user desktop environment in the game.
+ * 
+ * @author Austin Hwang
+ * @date 13 March 2026
+ */
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UserDesktopManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject desktop_interface;
+    [SerializeField]
+    private GameObject wifi_icon;
+    [SerializeField]
+    private GameObject vpn_status_text;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
